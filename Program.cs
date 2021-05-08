efa843dfd8e1c8bfde9618e85c2f614fb94b1d37
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-05-08 18:32:31 -0400

    NLog Demo Program.cs
    Program.cs for NLog Demo.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NLog;
+namespace NLogDemo
+{
+
+    public static class Program
+    {
+        public static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+
+        public static void Main()
+        {
+            try
+            {
+            logger.Debug("This is a debug message");
+            logger.Error("This is an error message");
+            logger.Fatal("This is a fatal message");
+            logger.Info("Hello World!");
+            System.Console.ReadKey();
+
+
+            }
+            catch (Exception ex)
+            {
+            logger.Error(ex, "Goodbye cruel world");
+            }
+        }
+    }  
+}
